6a9ab6c34fc0decd7bd0ed490d85ccc53800bd96
Viktor Rodau <111456511+Rodauv@users.noreply.github.com>
2024-06-25 12:28:42 +0200

    S4W6_Corrected code according to pylint

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,22 +1,24 @@
-def CalculateTotal(cart):
+"""Module summing and displaying shopping chart items and total values"""
+
+def calculatetotal(cart):
+    """Function to calculate total"""
     total = 0
-    total_items = len(cart)
-    for item in cart:
-        total += item['price']
+    for element in cart:
+        total += element['price']
     return total
 
-def display_total(Total):
-    print("Total price: " + Total)
+def displaytotal(total):
+    """Function to print total values"""
+    print("Total price: " + str(total))
 
 CART = [
     {'name': 'Item A', 'price': 10.99},
     {'name': 'Item B', 'price': 5.99},
-    {'name': 'Item C', 'price': '8.49'}
+    {'name': 'Item C', 'price': 8.49}
 ]
 
 for item in CART:
     print(f"Item: {item['name']} - Price: ${item['price']}")
 
-shopping_cart_total = CalculateTotal(CART)
-display_total(shopping_cart_total)
-
+shopping_cart_total = calculatetotal(CART)
+displaytotal(shopping_cart_total)
